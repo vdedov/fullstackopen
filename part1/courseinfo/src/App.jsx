0086a110339f9f55b40987de21caebd2c6a0e8a3
vdedov <dedov.tech@google.com>
2026-07-27 17:13:38 +0200

    1.4: Course Information step 4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -30,24 +30,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-    const part1 = {
+  const parts = [
+    {
       name: 'Fundamentals of React',
       exercises: 10
-    }
-    const part2 = {
+    },
+    {
       name: 'Using props to pass data',
       exercises: 7
-    }
-    const part3 = {
+    },
+    {
       name: 'State of a component',
       exercises: 14
     }
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total part1={part1} part2={part2} part3={part3}/>
+      <Content part1={parts[0]} part2={parts[1]} part3={parts[2]}/>
+      <Total part1={parts[0]} part2={parts[1]} part3={parts[2]}/>
     </div>
   )
 }
