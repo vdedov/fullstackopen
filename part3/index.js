be4f44d6a38d235281970e23e69762ef023a9582
vdedov <dedov.tech@google.com>
2026-08-01 20:23:59 +0200

    3.9: Phonebook bachend

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+app.use(express.static('dist'))
 
 app.use(express.json())
 
@@ -109,7 +111,7 @@ app.post('/api/persons', (request, response) => {
   response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
